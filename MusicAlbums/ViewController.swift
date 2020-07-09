0740646a656a0705a2a0b226b9c870580f080507
Jonas Simkus <jonauz@gmail.com>
2020-07-09 16:11:24 +0100

    Initial Commit

---
+++ MusicAlbums/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicAlbums
+//
+//  Created by Jonas Simkus on 09/07/2020.
+//  Copyright © 2020 Jonas Simkus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
